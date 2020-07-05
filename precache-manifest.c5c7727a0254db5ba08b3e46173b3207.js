af218074b82091d0b22ee69f9334b2e787a7b418
samjones310 <samjones31098@gmail.com>
2020-07-05 23:30:48 +0530

    Updates

---
+++ precache-manifest.c5c7727a0254db5ba08b3e46173b3207.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef92b7e0200a3269f829a0c4d93bac5",
+    "url": "/github-pages-ui/index.html"
+  },
+  {
+    "revision": "8919a5f7f22a1fc76b69",
+    "url": "/github-pages-ui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "08afea2aaccd6bdd9df0",
+    "url": "/github-pages-ui/static/js/2.a98e4e57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-ui/static/js/2.a98e4e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8919a5f7f22a1fc76b69",
+    "url": "/github-pages-ui/static/js/main.c6c5718c.chunk.js"
+  },
+  {
+    "revision": "52275133b5284d086647",
+    "url": "/github-pages-ui/static/js/runtime-main.659f9771.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
